c5bc24e6dd6da6d999e2f54a0adcffc6a9d32827
Dinh Loc Ngo <ngodinhloc@gmail.com>
2017-05-01 22:32:25 +1000

    #28 update ckeditor autogrow plugin: autogrow on startup

--- plugins/ckeditor/config_admin.js
+++ plugins/ckeditor/config_admin.js
@@ -3,22 +3,23 @@
  * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	config.toolbarGroups = [
-	                		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools']},
-	                		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-	                		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-	                		{ name: 'forms' },
-	                		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-	                		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-	                		{ name: 'links' },
-	                		{ name: 'insert' },
-	                		{ name: 'styles' },
-	                		{ name: 'colors' },
-	                		{ name: 'tools' },
-	                		{ name: 'others' }
-	                	];
-	config.removeButtons = 'Anchor,Strike,Subscript,Superscript';
-	config.extraPlugins = 'autogrow';
-	config.removePlugins= 'resize';
+CKEDITOR.editorConfig = function (config) {
+  config.toolbarGroups = [
+    {name: 'document', groups: ['mode', 'document', 'doctools']},
+    {name: 'clipboard', groups: ['clipboard', 'undo']},
+    {name: 'editing', groups: ['find', 'selection', 'spellchecker']},
+    {name: 'forms'},
+    {name: 'basicstyles', groups: ['basicstyles', 'cleanup']},
+    {name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi']},
+    {name: 'links'},
+    {name: 'insert'},
+    {name: 'styles'},
+    {name: 'colors'},
+    {name: 'tools'},
+    {name: 'others'}
+  ];
+  config.removeButtons = 'Anchor,Strike,Subscript,Superscript';
+  config.extraPlugins = 'autogrow';
+  config.removePlugins = 'resize';
+  config.autoGrow_onStartup = true;
 };
