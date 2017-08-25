ed55f3cb41e80dba5ef499705a3f0f8712772027
Dinh Loc Ngo <ngodinhloc@gmail.com>
2017-08-25 18:56:17 +1000

    #34 separate thedaysoflife classes; implement FacebookHelper

--- api/index.php
+++ api/index.php
@@ -10,14 +10,14 @@ use sys\Globals;
  * Sample api request
  * $hash is given to users when they registered for api service
  *
-$data    = ["userID" => 1000, "permission" => ["user", "day"]];
-$hash     = JWT::encode($data, Authentication::JWT_KEY_API);
-$request = ["hash"    => $hash,
-            "service" => "service_day",
-            "action"  => "get_day",
-            "para"    => ["id" => "100151", "json" => false,]];
-$url     = "www.thedaysoflife.com/api/?req=" . json_encode($request);
-$url     = 'www.thedaysoflife.com/api/?req={"hash":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySUQiOjEwMDAsInBlcm1pc3Npb24iOlsidXNlciIsImRheSJdfQ.f2ieaIQd8OrTK7UrA4BqDwhgg1NpzLV7OdOGIWBbQNU","service":"service_day","action":"get_day","para":{"id":"100151","json":false}}';
+$data = ["userID" => 1000, "permission" => ["user", "day"]];
+$token = JWT::encode($data, JWT_KEY_API);
+$req = ["token"   => $token,
+        "service" => "service_day",
+        "action"  => "get_day",
+        "para"    => ["id" => "100151", "json" => false,]];
+$url = "www.thedaysoflife.com/api/?req=" . json_encode($req);
+$url = 'www.thedaysoflife.com/api/?req={"token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySUQiOjEwMDAsInBlcm1pc3Npb24iOlsidXNlciIsImRheSJdfQ.f2ieaIQd8OrTK7UrA4BqDwhgg1NpzLV7OdOGIWBbQNU","service":"service_day","action":"get_day","para":{"id":"100151","json":false}}';
 */
 $req = Globals::get("req");
 $api = new API();
